a899e8b30ef498a0df9a46d4981049f63f80558e
badhan24 <ariful.chowdhury@gmail.com>
2022-12-26 15:42:28 -0500

    first push

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -1,19 +1,38 @@
 package math.problems;
 
+import static jdk.nashorn.internal.objects.Global.print;
+
 public class MakePyramid {
+
+    static int i;
+    static int j;
+    static int k;
+    static int number;
     public static void main(String[] args) {
-
         /*   Implement a large Pyramid of stars in the screen with java.
+         *
+         * *
+         * * *
+         * * * *
+         * * * * *
+         * * * * * *
+         */
 
-                              *
-                             * *
-                            * * *
-                           * * * *
-                          * * * * *
-                         * * * * * *
 
-        */
+
+
+        for(i=1; i<=number; i++)
+        {
+            for(j=i; j<number;j++)
+                System.out.print("I");
+            for(k=1; k<(i*2); k++)
+                System.out.print("*");
+            System.out.print("\n");
+
+        }
+
 
 
         }
+
   }
